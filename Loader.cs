2c3054c1f9498a4641db22b351b7ee968fe3a257
Mapleint <albertarlington21@gmail.com>
2020-07-05 15:29:40 -0700

    Structure, a new beginning of bean battle hvh

---
+++ Loader.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Collections;
+using System;
+using BeanAssembly.Features;
+
+namespace BeanAssembly
+{
+    // Loading Component
+    public class Loader
+    {
+        // Unity Loader
+        static GameObject gameObject;
+        public static void Load()
+        {
+            gameObject = new GameObject();
+            gameObject.AddComponent<syrup>();
+            UnityEngine.Object.DontDestroyOnLoad(gameObject);
+        }
+        public static void Unload()
+        {
+            UnityEngine.Object.Destroy(gameObject);
+        }
+    }
+}
+
+
---
+++ syrup.cs
@@ -0,0 +1,46 @@
+﻿using BeanAssembly.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BeanAssembly
+{
+    class syrup : MonoBehaviour
+    {
+        List<feature> features = new List<feature>();
+
+        void Start()
+        {
+            features.Add(new ESP());
+        }
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F1))
+                features[0].activated = !features[0].activated;
+            foreach (feature feat in features)
+                if(feat.activated)
+                    feat.Update();
+        }
+        void OnGUI()
+        {
+            //watermark
+            GUI.Label(new Rect(0, 0, 200, 200), "Syrup v0.1\nESP: " + (features[0].activated ? "On" : "Off"));
+
+            foreach (feature f in features)
+            {
+                if (f.activated)
+                    f.OnGUI();
+            }
+        }
+        void FixedUpdate()
+        {
+            foreach (feature feat in features)
+                if(feat.activated)
+                    feat.FixedUpdate();
+        }
+
+    }
+}
